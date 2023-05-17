8d815417e90564d998c39558dbbde868a923a69e
apurbojamananis <apurbojaman.anis@gmail.com>
2023-05-17 23:35:57 +0600

    firebase congfig update

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCvrZpMN8F7PeaxIKWdENqgM3f5hoQOvQk",
-  authDomain: "b7a11-toy-marketplace-client.firebaseapp.com",
-  projectId: "b7a11-toy-marketplace-client",
-  storageBucket: "b7a11-toy-marketplace-client.appspot.com",
-  messagingSenderId: "1046225304318",
-  appId: "1:1046225304318:web:eb02ed691be010e7de43af",
+  apiKey: import.meta.process.env.VITE_apiKey,
+  authDomain: import.meta.process.env.VITE_authDomain,
+  projectId: import.meta.process.env.VITE_projectId,
+  storageBucket: import.meta.process.env.VITE_storageBucket,
+  messagingSenderId: import.meta.process.env.VITE_messagingSenderId,
+  appId: import.meta.process.env.VITE_appId,
 };
 
 // Initialize Firebase
