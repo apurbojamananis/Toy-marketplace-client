9f2da93db1a40d1821c60ea692c79565b1ed7111
apurbojamananis <apurbojaman.anis@gmail.com>
2023-05-18 01:18:51 +0600

    error page update

--- src/components/Pages/Error/Error.jsx
+++ src/components/Pages/Error/Error.jsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
+
 const Error = () => {
   return (
-    <div>
-      <img src="./404.jpg" alt="" className="w-5/12 mx-auto" />
+    <div className="container mx-auto">
+      <img src="./404.jpg" alt="" className="w-6/12 mx-auto" />
+      <div className="w-6/12 mx-auto">
+        <button className="underline text-primary ">
+          <Link to="/">Back to Home</Link>
+        </button>
+      </div>
     </div>
   );
 };
